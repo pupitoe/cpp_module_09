0710cb602767a791012e75571b87ce658591f307
Tom Lasseret <tlassere@k1r1p5.42mulhouse.fr>
2024-06-17 17:32:50 +0200

    Feat: file ex01

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/17 17:23:00 by tlassere          #+#    #+#             */
+/*   Updated: 2024/06/17 17:32:09 by tlassere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# define SUCCESS 0
+# define FAIL 1
+
+
+
+#endif
