9021edc86453fb758533c01eb20c98fb27e66117
Tom Lasseret <tlassere@k1r1p8.42mulhouse.fr>
2024-06-21 20:34:48 +0200

    Feat: finish ex01

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 14:36:04 by tlassere          #+#    #+#             */
-/*   Updated: 2024/06/21 16:35:27 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/06/21 20:23:54 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 RPN::RPN(void)
 {
+	this->_last_action = PUSH_STACK;
 	return ;
 }
 
@@ -33,9 +34,95 @@ RPN& RPN::operator=(RPN const&)
 	return (*this);
 }
 
+void	RPN::add_stack(long nbr)
+{
+	if (this->_last_action == OPERATOR && this->_rpnizer.size() < 1)
+		throw RPN::BadFormat();
+	this->_last_action = PUSH_STACK;
+	this->_rpnizer.push(nbr);
+}
+
+void	RPN::add_op(void)
+{
+	long	buffer;
+
+	if (this->_rpnizer.size() >= 2)
+	{
+		buffer = this->_rpnizer.top();
+		this->_rpnizer.pop();
+		this->_rpnizer.top() += buffer;
+	}
+}
+
+void	RPN::subtract_op(void)
+{
+	long	buffer;
+
+	if (this->_rpnizer.size() >= 2)
+	{
+		buffer = this->_rpnizer.top();
+		this->_rpnizer.pop();
+		this->_rpnizer.top() -= buffer;
+	}
+}
+
+void	RPN::multiplie_op(void)
+{
+	long	buffer;
+
+	if (this->_rpnizer.size() >= 2)
+	{
+		buffer = this->_rpnizer.top();
+		this->_rpnizer.pop();
+		this->_rpnizer.top() *= buffer;
+	}
+}
+
+void	RPN::divisor_op(void)
+{
+	long	buffer;
+
+	if (this->_rpnizer.size() >= 2)
+	{
+		buffer = this->_rpnizer.top();
+		this->_rpnizer.pop();
+		if (buffer == 0)
+			throw RPN::BadFormat();
+		this->_rpnizer.top() /= buffer;
+	}
+}
+
+void	RPN::operator_stack(int const c)
+{
+	this->_last_action = OPERATOR;
+	if (this->_rpnizer.size() < 2)
+		throw RPN::BadFormat();
+	switch (c)
+	{
+		case '+':
+			this->add_op();
+			break;
+		case '-':
+			this->subtract_op();
+			break;
+		case '*':
+			this->multiplie_op();
+			break;
+		case '/':
+			this->divisor_op();
+			break;
+		default:
+			throw RPN::BadFormat();
+			break;
+	}
+}
+
 void	RPN::op_rpm(std::string const& c)
 {
-	std::cout << c << std::endl;
+	if (c.find_first_of("0123456789") < c.size())
+		this->add_stack(c[0] - '0');
+	else
+		this->operator_stack(c[0]);
 }
 
 void	RPN::use(std::string const& str)
@@ -51,6 +138,9 @@ void	RPN::use(std::string const& str)
 				this->op_rpm(str.substr(i, 1));
 			i++;
 		}
+		if (this->_rpnizer.size() != 1)
+			throw RPN::BadFormat();
+		std::cout << this->_rpnizer.top() << std::endl;
 	}
 	catch(const std::exception& e)
 	{
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 14:35:29 by tlassere          #+#    #+#             */
-/*   Updated: 2024/06/21 15:08:00 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/06/21 20:22:13 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,34 @@
 # include <stack>
 # include <exception>
 
+# define OPERATOR 0
+# define PUSH_STACK 1
+
 class	RPN
 {
 	private:
 		RPN(RPN const& cpy);
 		RPN& operator=(RPN const& cpy);
 
-		std::stack<long>	rpnizer;
+		std::stack<long>	_rpnizer;
+		int					_last_action;	
+
 		class	BadFormat: public std::exception
 		{
 			public:
 				const char	*what(void) const throw();
 		};
 
+		void	add_op(void);
+		void	subtract_op(void);
+		void	divisor_op(void);
+		void	multiplie_op(void);
+
+
 		void	op_rpm(std::string const& c);
-	
+		void	add_stack(long nbr);
+		void	operator_stack(int const c);
+
 	public:
 		RPN(void);
 		~RPN(void);
