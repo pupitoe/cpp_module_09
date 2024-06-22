74bac9e32b37c48397d783aeed46780bc2dc1150
Tom Lasseret <tlassere@k1r1p3.42mulhouse.fr>
2024-06-22 18:50:11 +0200

    Feat: ah

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,17 +6,22 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 21:16:54 by tlassere          #+#    #+#             */
-/*   Updated: 2024/06/22 15:38:44 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/06/22 18:49:44 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
+# include <ctime>
+
 int	main(int argc, char **argv)
 {
+	//std::clock_t;
 	if (argc > 1)
 	{
+		//std::time
 		ft_sort_vector(argc - 1, argv + 1);
+		//std::cout <<  << std::endl;
 	}
 	return (0);
 }
